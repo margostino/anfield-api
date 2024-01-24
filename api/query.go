d7ec347bfa70d90c9fbcdebecec3c59d8ed72410
Martín D'Agostino <maj.dagostino@gmail.com>
2024-01-24 21:59:18 +0100

    metric

--- api/query.go
+++ api/query.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/margostino/anfield-api/graph"
+	"github.com/margostino/anfield-api/metrics"
 	// "github.com/margostino/anfield-api/graph/generated"
 	// "github.com/margostino/anfield-api/metrics"
 )
@@ -12,7 +13,7 @@ import (
 var server = newServer()
 
 func Query(w http.ResponseWriter, r *http.Request) {
-	// go metrics.PublishRequest(r)
+	go metrics.PublishRequest(r)
 	server.ServeHTTP(w, r)
 }
 
